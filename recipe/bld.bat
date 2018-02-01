6ecd274c35810f7d9d9d0f18564b2b02a82471b9
xoviat <xoviat@users.noreply.github.com>
2018-02-01 15:46:09 -0600

    Show library prefix

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,13 +1,13 @@
 
-mkdir %LIBRARY_PREFIX%/include
-copy f2c.h %LIBRARY_PREFIX%/include
-mkdir %LIBRARY_PREFIX%/include/f2c
-copy f2c.h %LIBRARY_PREFIX%/include/f2c
+mkdir %LIBRARY_PREFIX%\include
+copy f2c.h %LIBRARY_PREFIX%\include
+mkdir %LIBRARY_PREFIX%\include\f2c
+copy f2c.h %LIBRARY_PREFIX%\include\f2c
 
 cd src
 
-copy makefile.vc Makefile
+nmake -f makefile.vc f2c.exe
 
-nmake f2c.exe
+copy f2c.exe %LIBRARY_PREFIX%\bin
 
-copy f2c.exe %LIBRARY_PREFIX%/bin
+dir %LIBRARY_PREFIX%\bin
