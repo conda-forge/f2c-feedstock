29b9fe87629eeeceab735b24aa906571e465e520
Alex Reustle <areustledev@gmail.com>
2022-04-07 12:19:14 -0400

    Compile the shared library and link with RPATH

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,6 +10,7 @@ patch arithchk.c ${RECIPE_DIR}/patch_arithchk
 # Using the makefile provided with the package
 # but adding the -fPIC option to the CFLAGS
 sed 's/CFLAGS = -O/CFLAGS = -O -fPIC/g' makefile.u > Makefile
+sed -i '' -e "s;\$(CC) -shared;\$(CC) -shared \$(CFLAGS) -Wl,-U,_MAIN__ -Wl,-rpath,${PREFIX}/lib ;g" Makefile
 
 # The Makefile directly calls "cc", which is sometimes not the name
 # of the compiler. So we create a link here and add this directory
@@ -20,18 +21,26 @@ export PATH=${PATH}:${PWD}
 make hadd
 make all
 
-# Make sure ${PREFIX}/lib exists, it might not since
+# Make sure ${PREFIX}/ dirs exist, they might not since
 # this package does not depend on anything
 mkdir -p ${PREFIX}/lib
+mkdir -p ${PREFIX}/include/f2c
 
-make install LIBDIR=${PREFIX}/lib 
+# Install the header and static library
+make install libdir=${PREFIX}/lib \
+  LIBDIR=${PREFIX}/lib \
+  includedir=${PREFIX}/include/f2c \
+  INCDIR=${PREFIX}/include/f2c
 
-# Some programs need f2c.h to be in ${PREFIX/include, some in ${PREFIX}/include/f2c
-# "make install" above does not install it, so we need to do it manually
-mkdir -p ${PREFIX}/include
-cp f2c.h ${PREFIX}/include
-mkdir ${PREFIX}/include/f2c
-cp f2c.h ${PREFIX}/include/f2c
+# Some programs need f2c.h to be in ${PREFIX}/include, some in ${PREFIX}/include/f2c
+# "make install" above does not reliably install it, so we need to do it manually.
+cp f2c.h ${PREFIX}/include/f2c.h
+cp f2c.h ${PREFIX}/include/f2c/f2c.h
+
+# Make the shared library explicity. It's neither part of the "all" target, nor the
+# "install" target.
+make libf2c.so
+cp libf2c.so ${PREFIX}/lib/libf2c.so
 
 # Now build the f2c executable
 cd ../src
@@ -46,5 +55,5 @@ cp f2c ${PREFIX}/bin/f2c
 
 # Install the pkg-config file
 mkdir -p ${PREFIX}/lib/pkgconfig
-echo "Version: ${PKG_VERSION}" >> ${RECIPE_DIR}/f2c.pc
 cp ${RECIPE_DIR}/f2c.pc ${PREFIX}/lib/pkgconfig/f2c.pc
+echo "Version: ${PKG_VERSION}" >> ${PREFIX}/lib/pkgconfig/f2c.pc
